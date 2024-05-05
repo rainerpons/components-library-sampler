acb735be8e31711bb03fe73094351f610aaeb84a
rainerpons <rainerpons11@gmail.com>
2024-05-05 00:03:17 -0700

    Added search forms and pagination buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
-import { useState } from 'react';
+import { Form, Input, Pagination } from "antd";
+import { ChangeEvent, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [textInput, setTextInput] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTextInput(event.target.value);
+  }
 
   return (
     <>
       <h1>Components Library Sampler</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
+        <Form>
+          <Form.Item label="Basic usage">
+            <Input placeholder="Search" value={textInput} onChange={handleChange} />
+          </Form.Item>
+          <Form.Item label="Search box">
+            <Input.Search
+              placeholder="Search"
+              value={textInput}
+              onChange={handleChange}
+            />
+          </Form.Item>
+          <Form.Item label="Search box #2">
+            <Input.Search
+              placeholder="Search"
+              value={textInput}
+              onChange={handleChange}
+              enterButton
+            />
+          </Form.Item>
+          <Form.Item label="Search box #3">
+            <Input.Search
+              placeholder="Search"
+              enterButton="Search"
+              size="large"
+              value={textInput}
+              onChange={handleChange}
+            />
+          </Form.Item>
+        </Form>
+        <Pagination total={100} />
       </div>
     </>
   );
