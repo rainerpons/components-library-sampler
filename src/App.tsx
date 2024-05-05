920d03f9ec10daa95e5e29ef8322bcb9bdb7f0bd
rainerpons <rainerpons11@gmail.com>
2024-05-04 23:29:24 -0700

    Text field and pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useState } from 'react';
+import { Pagination, TextField } from '@mui/material';
+import { ChangeEvent, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [inputText, setInputText] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
 
   return (
     <>
       <h1>Components Library Sampler</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
+        <TextField label="Search" value={inputText} onChange={handleChange} />
+        <Pagination count={10} />
       </div>
     </>
   );
